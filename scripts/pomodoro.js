b194fb4c0bc3539a520060f1e01ba668ea7d698b
ianashby <ianashbyphotography@gmail.com>
2022-05-25 18:06:54 -0600

    05/25

--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -3,6 +3,8 @@ const input = document.querySelector("input");
 const button = document.querySelector("#submit");
 const list = document.querySelector("ul");
 
+let tasks = [];
+
 // Create an click event listener for the Add Chapter button using addEventListener and an anonymous function.
 button.addEventListener("click", () => {
     // In the function block for adding a chapter, make sure the input is not blank and if not, do the following:
@@ -12,12 +14,20 @@ button.addEventListener("click", () => {
         const listItem = document.createElement("li");
         const listText = document.createElement("span");
         const deleteButton = document.createElement("img");
+        const checkBox = document.createElement('img');
 
+        // create checkbox
+        checkBox.setAttribute('src', '/images/check-square.svg')
+        checkBox.style.filter = 'invert(1)';
+        checkBox.style.cursor = 'pointer';
+        listItem.appendChild(checkBox);
         // Append elements to li.
         listText.innerHTML = input.value;
         listItem.appendChild(listText);
         // deleteButton.createElement('img');
         deleteButton.setAttribute('src', '/images/x.svg')
+        deleteButton.style.filter = 'invert(1)';
+        deleteButton.style.cursor = 'pointer';
         listItem.appendChild(deleteButton);
 
         // Append li to ul.
@@ -25,8 +35,28 @@ button.addEventListener("click", () => {
 
         // Create an event listener for the delete button that removes the li.
         deleteButton.addEventListener("click", () => {
-        list.removeChild(listItem);
+            list.removeChild(listItem);
         });
+
+        // line through checked off items
+        checkBox.addEventListener('click', () => {
+            listItem.style.textDecoration = 'line-through';
+            listItem.style.color = 'red';
+        });
+
+        // change checkbox on hover
+        checkBox.addEventListener('mouseover', () => {
+            checkBox.setAttribute('src', '/images/check-square-fill.svg');  
+        });
+
+        // change checkbox on hover off
+        checkBox.addEventListener('mouseout', () => {
+            checkBox.setAttribute('src', '/images/check-square.svg');  
+        });
+
+        // FOR CONSOLE: push items to an array to view
+        tasks.push(input.value);
+        console.log(tasks);
 
         // Focus and clear input.
         input.focus();
@@ -34,6 +64,33 @@ button.addEventListener("click", () => {
     }
 });
 
+const formatRemainingTime = (time) => {
+    let minutes = Math.floor(time/60);
+    let seconds = time % 60;
 
-// better to do list: https://freshman.tech/todo-list/
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
+
+    return `${minutes}:${seconds}`
+}
+
+let pomodoro_limit = 1500;
+let timePassed = 0;
+let timeLeft = pomodoro_limit;
+
+const startButton = document.querySelector('#start');
+
+startButton.addEventListener('click', () => {
+    setInterval(() => {
+    
+        // The amount of time passed increments by one
+        timePassed = timePassed += 1;
+        timeLeft = pomodoro_limit - timePassed;
+        
+        // The time left label is updated
+        document.getElementById('remaining-time').innerHTML = formatRemainingTime(timeLeft);
+      }, 1000);
+})
+
 // timer: https://css-tricks.com/how-to-create-an-animated-countdown-timer-with-html-css-and-javascript/
